b5b45b0da3f6ca2ed228c8476a72d97ccb14262c
TuberAsk <125912730+TuberAsk@users.noreply.github.com>
2023-11-21 12:01:17 +0100

    Update main.py

--- build/lib/SyntaxHighlighterS/main.py
+++ build/lib/SyntaxHighlighterS/main.py
@@ -1,7 +1,6 @@
 import re
 
 class SyntaxHighlighter:
-
     def __init__(self, code_text, language):
         self.code_text = code_text
         self.language = language
@@ -11,75 +10,58 @@ class SyntaxHighlighter:
         keywords = self.get_keywords_for_language(self.language)
         pattern = self.create_pattern(keywords)
 
-        # Remove previous tags
         for tag in ["keyword", "string", "print", "()", "numbers", "[]", "<>", "{}"]:
             self.code_text.tag_remove(tag, "1.0", "end")
 
-        # Find and tag all occurrences of keywords
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(pattern, line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("keyword", start_index, end_index)
                 self.code_text.tag_config("keyword", foreground="blue")
-
-        # Find and tag all occurrences of strings
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'(["\'])(?:(?=(\\?))\2.)*?\1', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("string", start_index, end_index)
                 self.code_text.tag_config("string", foreground="orange")
-
-        # Find and tag all occurrences of 'print'
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'\bprint\b', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("print", start_index, end_index)
                 self.code_text.tag_config("print", foreground="salmon")
-
-        # Find and tag all occurrences of numbers
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'\b\d+\b', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("numbers", start_index, end_index)
                 self.code_text.tag_config("numbers", foreground="purple")
-
-#-------------------------------------------------------------------------------------
-        # Find and tag all occurrences of '()' parentheses
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'[\{\}]', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("{}", start_index, end_index)
                 self.code_text.tag_config("{}", foreground="light blue")
-                
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'[\[\]]', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("[]", start_index, end_index)
                 self.code_text.tag_config("[]", foreground="yellow")
-        
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'[\<\>]', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("<>", start_index, end_index)
                 self.code_text.tag_config("<>", foreground="light blue")
-                
         for line_number, line in enumerate(code.split("\n"), start=1):
             for match in re.finditer(r'[\(\)]', line):
                 start_index = f"{line_number}.{match.start()}"
                 end_index = f"{line_number}.{match.end()}"
                 self.code_text.tag_add("()", start_index, end_index)
                 self.code_text.tag_config("()", foreground="yellow")
-#-------------------------------------------------------------------------------------
-
     def get_keywords_for_language(self, language):
-        # Define keywords for each supported language
         keywords_dict = {
             "C": ["if", "include", "signed", "static", "else", "for", "while", "int", "return", "void", "struct", "goto", "do", "char", "double", "short", "long", "extern", "union", "float", "auto", "break", "enum", "register", "typedef", "switch", "sizeof", "volatile", "default", "unsigned", "const", "continue", "case"],
             "Python": ["if", "else", "for", "while", "def", "import", "return", "class", "elif", "as", "except", "try"],
@@ -92,3 +74,5 @@ class SyntaxHighlighter:
 
     def create_pattern(self, keywords):
         return r'\b(?:' + '|'.join(keywords) + r')\b'
+
+    # Ignore the fact that the code might be a little messy. If you want to contribute consider doing so. :D
